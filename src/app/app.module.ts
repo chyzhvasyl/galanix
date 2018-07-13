917e2fcb00d708a9b7170644942255c733e34134
Chyzh Vasyl <vasyachyzh1996@gmail.com>
2018-07-13 23:17:48 +0300

    first task completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
+import { ThirdComponent } from './components/third/third.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
+
   ],
   imports: [
     BrowserModule
