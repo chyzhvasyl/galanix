41421ce9005ead82869ad1dc351f0fccec483e31
Chyzh Vasyl <vasyachyzh1996@gmail.com>
2018-07-16 02:25:08 +0300

    added delet localy

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,6 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {NgZone} from '@angular/core';
 import * as $ from 'jquery';
+import index from '@angular/cli/lib/cli';
+
+
+
+const images = [
+  {'id': 1, 'category': 'mood', 'url': 'assets/1.jpg ' },
+  {'id': 2, 'category': 'mood', 'url': 'assets/2.jpg ' },
+  {'id': 3, 'category': 'mood', 'url': 'assets/3.jpg ' },
+  {'id': 4, 'category': 'mood', 'url': 'assets/4.jpg ' },
+
+  {'id': 5, 'category': 'vibes', 'url': 'assets/5.jpg' },
+  {'id': 6, 'category': 'vibes', 'url': 'assets/6.jpg' },
+  {'id': 7, 'category': 'vibes', 'url': 'assets/7.jpg' },
+  {'id': 8, 'category': 'vibes', 'url': 'assets/8.jpg' },
+
+  {'id': 9, 'category': 'vibes', 'url': 'assets/9.jpg' },
+  {'id': 10, 'category': 'vibes', 'url': 'assets/10.jpg' },
+  {'id': 11, 'category': 'vibes', 'url': 'assets/11.jpg' },
+  {'id': 12, 'category': 'vibes', 'url': 'assets/12.jpg' }
+];
+
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
@@ -9,22 +30,8 @@ import * as $ from 'jquery';
 export class SecondComponent implements OnInit {
   mobWidth: number;
      date = new Date();
-    images = [
-    {'id': 1, 'category': 'mood', 'url': 'assets/1.jpg ' },
-    {'id': 2, 'category': 'mood', 'url': 'assets/2.jpg ' },
-    {'id': 3, 'category': 'mood', 'url': 'assets/3.jpg ' },
-    {'id': 4, 'category': 'mood', 'url': 'assets/4.jpg ' },
-
-    {'id': 5, 'category': 'vibes', 'url': 'assets/5.jpg' },
-    {'id': 6, 'category': 'vibes', 'url': 'assets/6.jpg' },
-    {'id': 7, 'category': 'vibes', 'url': 'assets/7.jpg' },
-    {'id': 8, 'category': 'vibes', 'url': 'assets/8.jpg' },
-
-      {'id': 9, 'category': 'vibes', 'url': 'assets/9.jpg' },
-      {'id': 10, 'category': 'vibes', 'url': 'assets/10.jpg' },
-      {'id': 11, 'category': 'vibes', 'url': 'assets/11.jpg' },
-      {'id': 12, 'category': 'vibes', 'url': 'assets/12.jpg' }
-  ];
+      
+images = images;
   constructor(ngZone: NgZone) {
     window.onresize = (e) => {
       ngZone.run(() => {
@@ -65,5 +72,11 @@ public  ngOnInit() {
 
   );
   }
+delete(image: any) {
+    let index = this.images.indexOf(image);
 
+if (index > -1) {
+  this.images.splice(index, 1);
+}
+}
 }
