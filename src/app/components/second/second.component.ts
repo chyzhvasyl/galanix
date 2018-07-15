fd9b90c90021cfb09c817495e018ee8666425e71
Chyzh Vasyl <vasyachyzh1996@gmail.com>
2018-07-16 01:55:37 +0300

    second task completed withot bonus

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgZone} from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
-
-
-
 export class SecondComponent implements OnInit {
+  mobWidth: number;
      date = new Date();
     images = [
     {'id': 1, 'category': 'mood', 'url': 'assets/1.jpg ' },
@@ -26,10 +25,45 @@ export class SecondComponent implements OnInit {
       {'id': 11, 'category': 'vibes', 'url': 'assets/11.jpg' },
       {'id': 12, 'category': 'vibes', 'url': 'assets/12.jpg' }
   ];
+  constructor(ngZone: NgZone) {
+    window.onresize = (e) => {
+      ngZone.run(() => {
+        this.mobWidth = window.innerWidth;
+      });
+    };
+  }
 
-  constructor() { }
+public  ngOnInit() {
+  $(document).ready(function() {
+    $('.img-fluid').click(function() {
+      const img = $(this);
+      const src = img.attr('src');
+      $('body').append('<div class=\'popup\'>' +
+        '<div class=\'popup_bg\'><div style=\"\" class =\'close\'>close</div></div>' +
+        '<img src=\'' + src + '\' class=\'popup_img\' />'  +
+        '</div>');
+      $('.popup').fadeIn(800);
+      $('.close').click(function() {
+        $('.popup').fadeOut(800);
+        setTimeout(function() {
+          $('.popup').remove();
+        }, 800);
+      });
+    }
 
-  ngOnInit() {
+    );
+      /*function showModule() {
+        $('#module').show('slow');
+      }
+      $(function() {
+        $('#close').click(function() {
+          $('#module').hide(200);
+        });
+
+      });*/
+  }
+
+  );
   }
 
 }
